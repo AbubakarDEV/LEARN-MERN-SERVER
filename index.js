d2f62209dcc56b7e1adf7ff7aac28597d80fb0fa
abubakarDev <abub1156@gmail.com>
2022-08-24 19:46:06 +0500

    re deploy

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
-app.get("/", (req, res) => {
-  res.send("Hello to heroku");
-});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
